458a475bc6f64e8f82c3f554ffa65607d88f7812
Wangley Vieira <wangleymsv10@hotmail.com>
2021-02-08 21:59:11 -0300

    Adicionado função Novo, Editar e Salvar

--- app/Http/Controllers/NoticiaController.php
+++ app/Http/Controllers/NoticiaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Noticia;
+use App\Models\Categoria;
 
 class NoticiaController extends Controller
 {
@@ -12,5 +13,28 @@ class NoticiaController extends Controller
         $noticias = Noticia::all();
 
         return view("noticia.listagem", compact('noticias'));
+    }
+
+    function novo(){
+        $noticia = new Noticia();
+        $noticia->id = 0;
+        $noticia->titulo="";
+        $noticia->descricao="";
+        $noticia->data="";
+        $noticia->cidade="";
+        $noticia->categoria=0;
+
+        $categorias = Categoria::all();
+        return view("noticia.formulario", compact('noticia', 'categorias'));
+    }
+
+    function editar($id){
+        $noticia = Noticia::find($id); //busca categorias no banco através do ID
+        $categorias = Categoria::all();
+        return view("noticia.formulario", compact('noticia', 'categorias'));
+    }
+
+    function salvar(Request $request){
+        return "salvando...";
     }
 }
